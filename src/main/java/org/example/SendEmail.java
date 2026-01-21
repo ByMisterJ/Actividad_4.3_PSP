d2190b08b4fdd60c8aa9e445784e71e3c5f957ad
josarapar <josearanda9000@gmail.com>
2026-01-21 15:59:38 +0100

    Add initial project structure with email functionality and configuration files

---
+++ src/main/java/org/example/SendEmail.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+
+public class SendEmail {
+
+    public void mandarCorreo() {
+        // El correo gmail de envío
+        String correoEnvia = "user";
+        String claveCorreo = "password";
+
+        // La configuración para enviar correo
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.user", correoEnvia);
+        properties.put("mail.password", claveCorreo);
+
+        // Obtener la sesion
+        Session session = Session.getInstance(properties, null);
+
+        try {
+            // Crear el cuerpo del mensaje
+            MimeMessage mimeMessage = new MimeMessage(session);
+
+            // Agregar quien envía el correo
+            mimeMessage.setFrom(new InternetAddress(correoEnvia));
+
+            // Los destinatarios
+            InternetAddress[] internetAddresses = {
+                    new InternetAddress("para1@gmail.com"),
+                    new InternetAddress("para2@gmail.com")};
+
+            // Agregar los destinatarios al mensaje
+            mimeMessage.setRecipients(Message.RecipientType.TO,
+                    internetAddresses);
+
+            // Agregar el asunto al correo
+            mimeMessage.setSubject("Subject");
+
+            // Creo la parte del mensaje
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setText("Body.");
+
+            // Crear el multipart para agregar la parte del mensaje anterior
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            // Agregar el multipart al cuerpo del mensaje
+            mimeMessage.setContent(multipart);
+
+            // Enviar el mensaje
+            Transport transport = session.getTransport("smtp");
+            transport.connect(correoEnvia, claveCorreo);
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Acabo de enviar un correo desde " + correoEnvia);
+    }
+
+    public static void main(String[] args) {
+        SendEmail correoTexto = new SendEmail();
+        correoTexto.mandarCorreo();
+    }
+}
